f97ccbf1f69049332d7f5cf29c26b045da603688
Francisco Franco <franciscofranco.1990@gmail.com>
2017-09-06 16:46:25 +0000

    Don't hardcode fstab, just patch it to minimize issues, sign the boot.img and update supersu's PATH
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -27,12 +27,18 @@ dump_boot;
 
 # begin ramdisk changes
 
+# fstab.shamu
+insert_line fstab.shamu "data        f2fs" after "data        ext4" "/dev/block/platform/msm_sdcc.1/by-name/userdata     /data           f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr                 wait,encryptable=/dev/block/platform/msm_sdcc.1/by-name/metadata";
+insert_line fstab.shamu "cache       f2fs" after "cache       ext4" "/dev/block/platform/msm_sdcc.1/by-name/cache        /cache          f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr                 wait,check";
+patch_fstab fstab.shamu /system ext4 flags "wait,verify=/dev/block/platform/msm_sdcc.1/by-name/metadata" "wait";
+patch_fstab fstab.shamu /data ext4 flags  "wait,check,formattable,forceencrypt=/dev/block/platform/msm_sdcc.1/by-name/metadata" "wait,check,formattable,encryptable=/dev/block/platform/msm_sdcc.1/by-name/metadata";
+
 # init.shamu.power.rc
 insert_line init.shamu.rc "init.fk.rc" after "import init.shamu.diag.rc" "import init.fk.rc";
 insert_line init.shamu.rc "performance_profiles" after "import init.shamu.diag.rc" "import init.performance_profiles.rc";
 replace_section init.shamu.rc "service mpdecision" " " "#service mpdecision /system/bin/mpdecision --avg_comp\n#   class main\n#   user root\n#   group root readproc\n#    writepid /dev/cpuset/system-background/tasks\n#   disabled\n";
-replace_string init.shamu.rc "#    verity_load_state" "    verity_load_state" "#    verity_load_state"
-remove_line init.shamu.power.rc "on property:dev.bootcomplete=1"
+replace_string init.shamu.rc "#    verity_load_state" "    verity_load_state" "#    verity_load_state";
+remove_line init.shamu.power.rc "on property:dev.bootcomplete=1";
 # end ramdisk changes
 
 write_boot;
