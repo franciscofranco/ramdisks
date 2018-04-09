c8724dc69b3874c620e27701fe79cbed1e7d3369
Francisco Franco <franciscofranco.1990@gmail.com>
2018-04-09 18:15:57 +0000

    Sync with r3
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- ramdisk/overlay/init.special_power.sh
+++ ramdisk/overlay/init.special_power.sh
@@ -27,9 +27,6 @@ chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_val
 chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 
 # gpu
 chmod 0664 /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
@@ -40,11 +37,12 @@ write /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay 0
 write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 100
 write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 0
 write /sys/devices/system/cpu/cpufreq/interactive/target_loads "75 1401600:60 1689600:70 1958400:85 2016000:95"
-write /sys/devices/system/cpu/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpufreq/interactive/timer_rate 30000
+write /sys/devices/system/cpu/cpufreq/interactive/min_sample_time 30000
 write /sys/devices/system/cpu/cpufreq/interactive/ignore_hispeed_on_notif 0
+write /sys/devices/system/cpu/cpufreq/interactive/io_is_busy 1
 
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:1036800"
+write /sys/module/cpu_boost/parameters/input_boost_freq "0:1401600"
 write /sys/module/cpu_boost/parameters/input_boost_ms 1500
 
 }&
