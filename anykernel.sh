a54c297da30a78e78b327aa13aae07cd28b3e0f2
Francisco Franco <franciscofranco.1990@gmail.com>
2018-09-17 03:20:56 +0000

    Updates
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -3,7 +3,7 @@
 
 ## AnyKernel setup
 # begin properties
-properties() {
+properties() { '
 kernel.string=Franco Kernel by franciscofranco @ xda-developers
 do.devicecheck=1
 do.modules=0
@@ -12,55 +12,45 @@ do.cleanuponabort=1
 device.name1=tissot
 device.name2=Mi A1
 device.name3=tissot_sprout
-} # end properties
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
-
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
 # sepolicy
 $bin/magiskpolicy --load sepolicy --save sepolicy \
   "allow init rootfs file execute_no_trans" \
 ;
 
-if [ -d $ramdisk/.subackup -o -d $ramdisk/.backup ]; then
-  patch_cmdline "skip_override" "skip_override";
-else
-  patch_cmdline "skip_override" "";
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D6673 77616E745F696E697472616D6673;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
 fi;
-
-if [ -d $ramdisk/.backup ]; then
-  overlay=$ramdisk/overlay;
-elif [ -d $ramdisk/.subackup ]; then
-  overlay=$ramdisk/boot;
-fi;
-
-list="init.rc";
-for rdfile in $list; do
-  rddir=$(dirname $rdfile);
-  mkdir -p $overlay/$rddir;
-  test ! -f $overlay/$rdfile && cp -rp /system/$rdfile $overlay/$rddir/;
-done;
-
-insert_line $overlay/init.rc "init.fk.rc" before "import /init.usb.rc" "import /init.fk.rc";
-insert_line $overlay/init.rc "performance_profiles" before "import /init.usb.rc" "import /init.performance_profiles.rc";
 
 # end ramdisk changes
 
