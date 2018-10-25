b53f8feaaea88f0d220f0ee276ee40809094ecbf
Francisco Franco <franciscofranco.1990@gmail.com>
2018-10-25 20:01:46 +0000

    Sync
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=franco.Kernel by franciscofranco @ xda-developers
+kernel.string=FrancoKernel by franciscofranco @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -15,7 +15,7 @@ device.name1=hammerhead
 # shell variables
 block=/dev/block/platform/msm_sdcc.1/by-name/boot;
 is_slot_device=0;
-
+ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
