550754048cd4e6318a196cc60d688d6e04ff0dda
Francisco Franco <franciscofranco.1990@gmail.com>
2018-12-20 21:48:03 +0000

    First push
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,58 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Franco Kernel by franciscofranco @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=beryllium
+supported.versions=9, 9.0
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+# Save the users from themselves
+android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
+supported_version=9;
+if [ "$android_version" != "$supported_version" ]; then
+  ui_print " "; ui_print "You are on Android $android_version but this kernel is only for Android $supported_version!";
+  exit 1;
+fi;
+
+## AnyKernel install
+dump_boot;
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi;
+
+# Install the boot image
+write_boot;
