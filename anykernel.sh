bb35dbb298c485034c482dcb2d61116ad4c4627a
Francisco Franco <franciscofranco.1990@gmail.com>
2019-01-12 03:35:56 +0000

    Sync for r20
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,47 +2,60 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# EDIFY properties
-properties() {
+# begin properties
+properties() { '
 kernel.string=FrancoKernel by franciscofranco @ xda-developers
 do.devicecheck=1
-do.initd=0
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=1
-device.name1=sailfish
-device.name2=marlin
-}
+do.cleanuponabort=0
+device.name1=marlin
+device.name2=sailfish
+'; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/624000.ufshc/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
+ramdisk_compression=auto;
 
-## end setup
 
+## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
+
 
 ## AnyKernel install
 dump_boot;
 
-if [ -d $ramdisk/boot/dev ]; then
-  patch_cmdline "skip_override" "skip_override";
-else
-  patch_cmdline "skip_override" "";
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.lz4
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=lz4 $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
 fi;
 
-# begin ramdisk changes
-# init.sailfish.rc
-# init.marlin.rc
 
-# boot/init.sailfish.rc
-backup_file boot/init.sailfish.rc;
-insert_line boot/init.sailfish.rc "init.fk" before 'import init.${ro.hardware}.nanohub.rc' "import init.fk.rc";
-insert_line boot/init.sailfish.rc "performance_profiles" before 'import init.${ro.hardware}.nanohub.rc' "import init.performance_profiles.rc";
-# end ramdisk changes
+# Patch dtbo on custom ROMs
+hostname="$(file_getprop /system/build.prop "ro.build.host")"
+case "$hostname" in
+    *corp.google.com|abfarm*) host=google;;
+    *) host=custom;;
+esac
+if [ "$(file_getprop /system/build.prop "ro.build.user")" != "android-build" -o "$host" == "custom" ]; then
+  ui_print " "; ui_print "You are on a custom ROM, patching dtb to remove verity...";
+  $bin/magiskboot --dtb-patch /tmp/anykernel/Image.lz4-dtb;
+else
+  ui_print " "; ui_print "You are on stock, not patching dtb to remove verity!";
+fi;
 
+# Install the boot image
 write_boot;
-
-## end install
-
